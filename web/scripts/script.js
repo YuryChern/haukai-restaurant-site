2f7a56eb382d8328b678e226af5aeb374bbde72a
YuryChern <yuryc@me.com>
2022-10-11 22:14:53 +1300

    Pretty Code

--- web/scripts/script.js
+++ web/scripts/script.js
@@ -1,14 +1,30 @@
 function GCal() {
-  window.open("calendar.html", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,width=400,height=400, left=200%, top=200%");
+  window.open(
+    "calendar.html",
+    "_blank",
+    "toolbar=yes,scrollbars=yes,resizable=yes,width=400,height=400, left=200%, top=200%"
+  );
 }
 function GMap() {
-  window.open("map.html", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,width=400,height=400, left=200%, top=200%");
+  window.open(
+    "map.html",
+    "_blank",
+    "toolbar=yes,scrollbars=yes,resizable=yes,width=400,height=400, left=200%, top=200%"
+  );
 }
 function privacy() {
-  window.open("privacy.html", "Privacy Statement", "toolbar=no,scrollbars=yes,resizable=yes,width=600,height=800,top=200%, left=200%");
+  window.open(
+    "privacy.html",
+    "Privacy Statement",
+    "toolbar=no,scrollbars=yes,resizable=yes,width=600,height=800,top=200%, left=200%"
+  );
 }
 function staff() {
-  window.open("private/private.html", "Staff Roster", "toolbar=no,scrollbars=yes,resizable=yes,width=800,height=600,top=200%, left=200%");
+  window.open(
+    "private/private.html",
+    "Staff Roster",
+    "toolbar=no,scrollbars=yes,resizable=yes,width=800,height=600,top=200%, left=200%"
+  );
 }
 function myFunction() {
   var x = document.getElementById("demo");
